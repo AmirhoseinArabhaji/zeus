44893efe76c7bac82b9d793b7dc2a332a4ddc2cd
Amirhosein Arabhaji <a.arab12345@gmail.com>
2022-06-26 17:35:38 +0430

    feat(todo): add view `TaskModelViewSet`

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import Task
+from .permissions import IsTaskOwner
+from .serializers import TaskSerializer
+
+
+class TaskModelViewSet(ModelViewSet):
+    serializer_class = TaskSerializer
+    permission_classes = (IsAuthenticated, IsTaskOwner)
+
+    def get_queryset(self):
+        return Task.objects.filter(owner=self.request.user)
